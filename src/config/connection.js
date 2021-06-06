f2d398955e6a3d0ca1cf4c8ed3fef7b9f3708dc9
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-06-06 13:19:14 +0100

    added express server

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,5 +1,5 @@
-const Sequelize = require("sequelize");
 require("dotenv").config();
+const Sequelize = require("sequelize");
 
 const dbOptions = {
   host: process.env.DB_HOST,
--- src/server.js
+++ src/server.js
@@ -1,1 +1,26 @@
-console.log("server connected");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const sequelize = require("./config/connection");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+const init = async () => {
+  try {
+    await sequelize.sync();
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.error("Failed to connect to DB");
+  }
+};
+
+init();
