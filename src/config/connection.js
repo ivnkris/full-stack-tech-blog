acf79e7a633c4203262cd0ccdb83e673da16e571
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-06-06 12:10:02 +0100

    added sequelize connection config and User model

---
+++ src/config/connection.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+
+const dbOptions = {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  port: process.env.DB_PORT,
+  logging: false,
+};
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+const connection = new Sequelize(dbName, dbUser, dbPassword, dbOptions);
+
+module.exports = connection;
---
+++ src/models/User.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+const options = {
+  sequelize,
+  modelName: "user",
+  timestamps: true,
+  underscored: true,
+  freezeTableName: true,
+};
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+class User extends Model {}
+
+User.init(schema, options);
+
+module.exports = User;
