211c6d2f3695302d2fb46a5e4c4eaf05f24818ff
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-06-06 12:20:41 +0100

    added method to validate password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 
 const hooks = require("../hooks");
 const sequelize = require("../config/connection");
@@ -46,7 +47,12 @@ const schema = {
   },
 };
 
-class User extends Model {}
+class User extends Model {
+  async isValidPassword(password) {
+    const isValid = await bcrypt.compare(password, this.password);
+    return isValid;
+  }
+}
 
 User.init(schema, options);
 
