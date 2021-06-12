f35f25f9eb277852d5b49865a6b6923b612828ea
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-06-12 16:15:36 +0100

    added logic to handleCreatePost controller (WIP)

--- src/controllers/api/handleCreatePost.js
+++ src/controllers/api/handleCreatePost.js
@@ -1,5 +1,17 @@
+const Post = require("../../models/Post");
+
 const handleCreatePost = (req, res) => {
-  res.send("created post");
+  try {
+    const newPost = Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      // TODO: user_id needed from session
+    });
+    res.json(newPost);
+  } catch (error) {
+    console.log(`[ERROR] - ${error.message}`);
+    res.status(500).json({ error: "Failed to create new post" });
+  }
 };
 
 module.exports = handleCreatePost;
