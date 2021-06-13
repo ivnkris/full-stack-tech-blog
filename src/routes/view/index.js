3ff371e5b198c94afe33274ddfab160e2125c350
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-06-13 11:00:22 +0100

    added path protection

--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -1,14 +1,12 @@
 const { Router } = require("express");
 
-const renderHomePage = require("../../controllers/view/renderHomePage");
-const renderLoginPage = require("../../controllers/view/renderLoginPage");
-const renderDashboard = require("../../controllers/view/renderDashboard");
+const publicRoutes = require("./public");
+const privateRoutes = require("./private");
+const auth = require("../../middleware/auth");
 
 const router = Router();
 
-router.get("/login", renderLoginPage);
-router.get("/dashboard", renderDashboard);
-
-router.get("/", renderHomePage);
+router.use(publicRoutes);
+router.use(auth, privateRoutes);
 
 module.exports = router;
---
+++ src/routes/view/public.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const renderHomePage = require("../../controllers/view/renderHomePage");
+const renderLoginPage = require("../../controllers/view/renderLoginPage");
+
+const router = Router();
+
+router.get("/login", renderLoginPage);
+router.get("/", renderHomePage);
+
+module.exports = router;
