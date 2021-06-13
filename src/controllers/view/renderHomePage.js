fba547b472cd068e03c45eca4ac15a217b90b17c
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-06-13 12:05:56 +0100

    posts rendering on homepage

--- src/controllers/view/renderHomePage.js
+++ src/controllers/view/renderHomePage.js
@@ -1,5 +1,29 @@
-const renderHomePage = (req, res) => {
-  res.render("homepage");
+const { Post, Comment, User } = require("../../models");
+
+const renderHomePage = async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      include: [
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+            },
+          ],
+        },
+        {
+          model: User,
+        },
+      ],
+    });
+
+    const formattedPosts = posts.map((post) => post.get({ plain: true }));
+    res.render("homepage", { posts: formattedPosts });
+  } catch (error) {
+    console.log(`[ERROR] - ${error.message}`);
+    res.status(500).json({ error: "Failed to render homepage" });
+  }
 };
 
 module.exports = renderHomePage;
