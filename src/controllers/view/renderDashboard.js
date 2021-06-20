71f0d72b496b561ed616aab8623707f7511823c8
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-06-20 11:50:52 +0100

    dashboard showing user blog posts and comments

--- src/controllers/view/renderDashboard.js
+++ src/controllers/view/renderDashboard.js
@@ -1,5 +1,48 @@
-const renderDashboard = (req, res) => {
-  res.render("dashboard");
+const { Post, User, Comment } = require("../../models");
+
+const renderDashboard = async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      where: {
+        user_id: req.session.userId,
+      },
+      include: [
+        {
+          model: User,
+        },
+        {
+          model: Comment,
+        },
+      ],
+    });
+
+    const comments = await Comment.findAll({
+      where: {
+        user_id: req.session.userId,
+      },
+      include: [
+        {
+          model: User,
+        },
+        {
+          model: Post,
+        },
+      ],
+    });
+
+    const formattedPosts = posts.map((post) => post.get({ plain: true }));
+    const formattedComments = comments.map((comment) =>
+      comment.get({ plain: true })
+    );
+    const payload = {
+      posts: formattedPosts,
+      comments: formattedComments,
+    };
+    res.render("dashboard", { payload });
+  } catch (error) {
+    console.log(`[ERROR] - ${error.message}`);
+    res.status(500).json({ error: "Failed to render dashboard" });
+  }
 };
 
 module.exports = renderDashboard;
