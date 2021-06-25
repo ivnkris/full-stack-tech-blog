7d0668f6c051a1a0680d3ad3506951e0533aeaf2
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-06-25 18:07:22 +0100

    added JAWSDB to connection.js

--- src/config/connection.js
+++ src/config/connection.js
@@ -12,6 +12,13 @@ const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
-const connection = new Sequelize(dbName, dbUser, dbPassword, dbOptions);
+let connection;
+if (process.env.JAWSDB_URL) {
+  // connection for heroku
+  connection = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // connection for local development
+  connection = new Sequelize(dbName, dbUser, dbPassword, dbOptions);
+}
 
 module.exports = connection;
